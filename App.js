023ee1c75c990feca9f8ca5683ec86988d3f66fe
akash <akashkumarvns7271@gmail.com>
2023-07-12 02:18:41 +0530

    input task done

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
-// const inputValue=document.querySelector(".inputfield");
-// const addTask=document.querySelector(".add-task");
-// const addedElement=document.querySelector(".added-element");
-// addTask.addEventListener("click",(e)=>{
-//     e.preventDefault();
+const inputValue=document.getElementById("todo");
+const addTask=document.querySelector(".add-task");
+const addedElement=document.querySelector(".added-element");
+addTask.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let innertext=inputValue.value;
+    // console.log(innertext);
+
    
     
-//     const newAddElement=document.createElement("li");
-//     newAddElement.className="item";
-//     newAddElement.setAttribute("draggable","true");
-//     newAddElement.textContent=`${inputValue}`;
-//     addedElement.appendChild(newAddElement);
+    const newAddElement=document.createElement("li");
+    newAddElement.className="item";
+    newAddElement.setAttribute("draggable","true");
+    newAddElement.textContent=`${innertext}`;
+    addedElement.appendChild(newAddElement);
 
 
-// });
+});
 
-const inputContainer=document.getElementById("form");
-const inputvalue=document.getElementById("input-value");
-const btn=document.getElementById("btn");
-inputContainer.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    const inputval=inputvalue
-})
+
+
 
 
 
