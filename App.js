c55363f2da13b9b19f7cbef7929735e1b64eab1a
akash <akashkumarvns7271@gmail.com>
2023-07-12 01:28:40 +0530

    drag done

--- App.js
+++ App.js
@@ -1,1 +1,29 @@
+// const inputValue=document.querySelector(".inputfield");
+// const addTask=document.querySelector(".add-task");
+// const addedElement=document.querySelector(".added-element");
+// addTask.addEventListener("click",(e)=>{
+//     e.preventDefault();
+   
+    
+//     const newAddElement=document.createElement("li");
+//     newAddElement.className="item";
+//     newAddElement.setAttribute("draggable","true");
+//     newAddElement.textContent=`${inputValue}`;
+//     addedElement.appendChild(newAddElement);
+
+
+// });
+
+const inputContainer=document.getElementById("form");
+const inputvalue=document.getElementById("input-value");
+const btn=document.getElementById("btn");
+inputContainer.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const inputval=inputvalue
+})
+
+
+
+
+
 
